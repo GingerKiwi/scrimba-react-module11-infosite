d0661a1fc65123f8cb2287512d4df16f7a578d07
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-06-08 04:02:39 -0400

    add intial html to Main.jsx and Navbar.jsx. add avatar.png to src/assets

---
+++ react-info-site/src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import avatar from './assets/avatar.png'
+
+export default function Main() {
+    return (
+        <div className="headshot">
+            <img src={avatar} alt="avatar" />
+        </div>
+    )
+}
+
