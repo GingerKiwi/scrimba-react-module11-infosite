444828d2ff3c79f922a166b7849fdc44ba4dfb41
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-06-08 04:36:17 -0400

    start working on css classes

--- react-info-site/src/Components/Main.jsx
+++ react-info-site/src/Components/Main.jsx
@@ -1,20 +1,20 @@
 export default function Main() {
     return (
         <article className="main">
-            <section>
+            <section className="section--dev-info">
                 <h1>Liz M<sup>c</sup>Cready</h1>
                 <h2>Frontend Developer</h2>
                 <p><a href="https://gingerkiwi.blog">https://gingerkiwi.blog</a></p>
             </section>
-            <section className="section--btn">
-                <button className="btn-github"><a href="http://github.com/gingerkiwi"></a>GitHub</button>
-                <button className="btn-linkedin"><a href="https://www.linkedin.com/in/elizabethmccready/"></a>LinkedIn</button>
+            <section className="section--btns">
+                <button className="btn--github"><a href="http://github.com/gingerkiwi"></a>GitHub</button>
+                <button className="btn--linkedin"><a href="https://www.linkedin.com/in/elizabethmccready/"></a>LinkedIn</button>
             </section>
-            <section>
+            <section className="section--info-paragraphs">
                 <h2>About</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur excepturi praesentium numquam molestiae modi exercitationem veritatis. Officia odit cupiditate culpa eaque, error sequi perferendis nihil nobis necessitatibus ex, provident ea.</p>
             </section>
-            <section>
+            <section className="section--info-paragraphs">
                 <h2>Interests</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque veritatis soluta, ipsum voluptates, dolores, nam eligendi aut dolor blanditiis non fugiat ab odit voluptatum vel enim. Illo sed corrupti tenetur.</p>
             </section>
